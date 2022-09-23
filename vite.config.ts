db0f1f33bd3154c0eccb76a3e779918f432b14b0
Seno <borlandseno@yahoo.com>
2022-09-23 14:39:53 +1200

    feat (electron): Electron integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from 'node:url';
+const path = require('path');
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    plugins: [vue()],
+    base: path.resolve(__dirname, './dist/'),
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
     },
-  },
 });
